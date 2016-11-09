540e3b0a3195d66ec692a23eee9fa0fadad78c37
Nikita Fedyashev <nfedyashev@gmail.com>
2016-11-09 03:02:48 +0200

    Add script for consul-template installation

---
+++ install-consul-template.sh
@@ -0,0 +1,6 @@
+wget https://releases.hashicorp.com/consul-template/0.16.0/consul-template_0.16.0_linux_amd64.zip
+cd /tmp
+unzip consul-template_0.16.0_linux_amd64.zip
+sudo chmod a+x consul-template
+sudo mv consul-template /usr/bin/consul-template
+
